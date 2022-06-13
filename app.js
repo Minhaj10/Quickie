4b09fd6c4e3ef7fb19b8e58ac07a6d198ab009de
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-13 23:15:13 +0600

    app.js and env file setup

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+dotenv.config();
+
+//database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Databade connecet successfully"))
+  .catch((err) => console.log(err));
+
+//request process
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+
+app.set("view engine", "ejs");
+
+//set static folders
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse cookies
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing setup
+
+//error handling
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening to port ${process.env.PORT}`);
+});
