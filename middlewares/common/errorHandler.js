49927bb55e481f2b1108a73790abf866b692627b
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-15 14:35:24 +0600

    error handler implemented

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -9,9 +9,18 @@ function notFoundHandler(req, res, next) {
 // default error handler
 
 function errorHandler(err, req, res, next) {
-  res.render("error", {
-    title: "Error page",
-  });
+  res.locals.error =
+    process.env.NODE_ENV === "development" ? err : { message: err.message };
+
+  res.status(err.status || 5000);
+
+  if (!res.locals.html) {
+    res.render("error", {
+      title: "error page",
+    });
+  } else {
+    res.json(res.locals.error);
+  }
 }
 
 module.exports = {
